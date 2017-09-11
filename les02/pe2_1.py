ea635b0eaaf88840988252ec2cb4702c7b873f0b
wolfofwallstreetH <HazemEngineer92>
2017-09-11 12:24:21 +0200

    Dit is tweede commit (test)

--- les02/pe2_1.py
+++ les02/pe2_1.py
@@ -1,4 +1,11 @@
 letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
-print(sorted(letters))
 
 
+aantalA = letters.count('A')
+print('aantalA = ' + str(aantalA))
+
+aantalB = letters.count('B')
+print('aantalB = ' + str(aantalB))
+
+aantalC = letters.count('C')
+print('aantalC =' +str(aantalC))
