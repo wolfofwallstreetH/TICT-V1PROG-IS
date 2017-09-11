40012f312ef66d55f2718d9ef748e6242302004c
wolfofwallstreetH <HazemEngineer92>
2017-09-11 20:43:24 +0200

    Dit is tweede commit (test)

---
+++ les03/pe3_1.py
@@ -0,0 +1,14 @@
+score = eval(input('geef je score: '))
+
+
+if (score > 15):
+    print()
+    print('gefeliciteerd!')
+    print()
+
+    print('Met een score van '+ str(score)+' ben je geslaagd')
+
+else:
+    print('onvoldoende')
+
+
