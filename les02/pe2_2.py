9b841724d4ca4005e5b8d2abf2da6ddbe64f848f
wolfofwallstreetH <HazemEngineer92>
2017-09-11 12:29:32 +0200

    Dit is tweede commit (test)

--- les02/pe2_2.py
+++ les02/pe2_2.py
@@ -6,4 +6,5 @@ totaalCijfersBlok1 = (cijferICOR + cijferPROG + cijferCSN)
 
 gemiddelde = (totaalCijfersBlok1 / 3)
 beloning = totaalCijfersBlok1 * 30
-overzicht = ('mijn cijfers ' + gemiddelde + ' leveren een beloning van ' + beloning)
+overzicht = ('mijn cijfers ' + str(gemiddelde) + ' leveren een beloning van ' + str(beloning) )
+print(overzicht)
